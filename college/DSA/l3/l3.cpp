67a25a506dbe7b011b12edfdc9024bfe0d04047d
sarvesh <sarveshss2513@gmail.com>
2024-08-31 09:19:18 +0530

    savresh

---
+++ college/DSA/l3/l3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        /* code */
+        for (int j = 0; j < 4; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ college/DSA/lecture1.md
@@ -0,0 +1,11 @@
+---
+id: "custom"
+---
+
+@import "note-style.less"
+
+<link href="https://fonts.googleapis.com/css2?family=Handlee&display=swap" rel="stylesheet">
+
+# Lecture 2
+
+### EXECUTION OF CODE
